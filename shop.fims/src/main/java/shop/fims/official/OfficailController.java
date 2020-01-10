9e413db7c2616b145ea85c0324bac2e93fef07ff
han <dusthgks@naver.com>
2020-01-10 17:19:27 +0900

    [team_04 Han-Soyeon] 부서조회 수정중_200110

--- shop.fims/src/main/java/shop/fims/official/OfficailController.java
+++ shop.fims/src/main/java/shop/fims/official/OfficailController.java
@@ -1,10 +1,17 @@
 package shop.fims.official;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import shop.fims.admin.AdminService;
 
 @Controller
 public class OfficailController {
+	
+	@Autowired private AdminService adminService;
+	@Autowired private OfficailService officailService;
 	
 	/*
 	 * @file   OfficailController.java
@@ -27,7 +34,12 @@ public class OfficailController {
 	
 	//행정기관별 부서 조회
 	@GetMapping("/official/DepartList")
-	public String DepartList() {
+	public String DepartList(Model areaCityAdminList, Model DepartList) {
+		
+		areaCityAdminList.addAttribute("areaCityAdminList", adminService.areaCityAdminList());
+		DepartList.addAttribute("DepartList", officailService.DepartList());
+		System.out.println("부서 조회 >>>>" + DepartList.toString());
+		
 		return "official/DepartList";
 	}
 	
