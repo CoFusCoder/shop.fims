33d7bff3fdf3c511f1affdcec1f8bca46b8a972c
JHY <JHY@121.186.99.253>
2020-01-12 22:44:46 +0900

    [team4]조윤상
    심사처리

--- shop.fims/src/main/java/shop/fims/estimate/EstimateMapper.java
+++ shop.fims/src/main/java/shop/fims/estimate/EstimateMapper.java
@@ -10,9 +10,22 @@ import shop.fims.vo.Estimate;
 @Mapper
 public interface EstimateMapper {
 	//심사대기 리스트
-	public List<String> estimatelist();
-	//심사명 선택
-	public List<Estimate> examinationIndex();
+	public List<Estimate> estimatelist();	
+	//리스트->신청서 페이지 변경
+	public List<Estimate> nextapply(String exa_rep_cd);
+	//신청서 ->심사 페이지 변경
+	public List<Estimate> examinationIndex(String exaRepCd);
+	//시험 문항
+	public List<Estimate> indexeva(String eva); 
+	//시험배점
+	public List<Estimate> evaList(String eva);
 	
-	public Estimate testSelect(String stnId);
+	//전체를 받는 곳
+	public Map<String,List<Estimate>> evaindexscocd(String eva);
+	
+	//심사완료후 승인 여부	
+	public List<Estimate> updateestimatelist(String exaRepCd);
+	//담당자 등록 (update)
+	public void updataexeManager(String exaRepCd,String exeManager);
+	
 }
